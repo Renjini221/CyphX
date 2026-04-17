3221a477a42225d2820421b0b999657eda307857
SREEPATHI B <renjini221@gmail.com>
2026-04-17 15:26:52 +0530

    addded one more security layer with the no- of words in domain

--- api/app.py
+++ api/app.py
@@ -1,8 +1,9 @@
 from flask import Flask,render_template,jsonify, request
 import requests
+import re
 app=Flask(__name__)
-import os
-apikey=os.environ.get("apikey")
+
+apikey="AIzaSyAs0QrXFT8PesofAl6XjUKGHZ1wc9X8iDQ"
 
 @app.route("/")
 def home():
@@ -36,6 +37,18 @@ def check():
     data = res.json()
 
     risk = 0
+
+    domain = url.split("/")[2]
+    vowels = sum(1 for c in domain if c in "aeiou")
+    consonants = sum(1 for c in domain if c.isalpha() and c not in "aeiou")
+    if consonants > 0 and vowels / (vowels + consonants)<0.25:
+          risk += 2
+
+    if len(domain) > 20:
+          risk += 1
+    if sum(c.isdigit() for c in domain)>3:
+          risk += 1
+                      
     if "matches" in data:
           return jsonify({"status":"danger","message":"Scam Btw"})
         
@@ -46,7 +59,7 @@ def check():
             risk+=1
     if "-" in url:
             risk+=1
-    if risk>=3:
+    if risk>=2:
             return jsonify({"status":"suspicious","message":"its a suspicious website"})
     else:
             return jsonify({"status":"safe","message":"safe"})
